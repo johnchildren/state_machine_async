1574afb868eb6f5a30a8a72a177d546c4b911fb9
John Children <john.a.children@gmail.com>
2019-08-22 08:58:06 +0100

    Add some commentary

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,16 @@
+/*!
+
+Experimenting with a [state_machine_future](https://github.com/fitzgen/state_machine_future) alternative using async/await.
+
+I'm trying to work out if it's still worth writing state machines through derive macros,
+so I wrote up one of the example state machines from `state_machine_future` in order to see what the ergonomics would be like.
+It seemed okay and potentially worthwhile so the next stage is writing the derive crate.
+
+
+Requires Rust Nightly 1.39+
+
+*/
+
 use async_trait::async_trait;
 use futures_core::future::BoxFuture;
 use futures_util::future::FutureExt;
